50309bd53e5b6a1795767660d7f1b3e2a10c8907
EDToaster <elongateddanishtoaster@gmail.com>
2020-10-07 17:45:50 -0700

    Finished

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ do
     dafny "$file" || { printf "⛔ There was a problem with verifying file $file\n"; exit 1; } 
 done
 
-NUMFILES=$(echo '$FILES' | wc -w)
+NUMFILES=$(echo "$FILES" | wc -w)
 printf "✔ Successfully verified $NUMFILES files.\n"
